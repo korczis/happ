3f48adb79f1c423dadd1b231a633f92399f1562c
Tomas Korcak <korczis@gmail.com>
2021-04-28 12:37:11 +0200

    Update dependencies

--- assets/js/components/Demo.tsx
+++ assets/js/components/Demo.tsx
@@ -1,15 +1,37 @@
 import React from "react";
 
+import {
+    MapContainer,
+    TileLayer,
+    Marker,
+    Popup
+} from 'react-leaflet';
+
+import { useWindowDimensions } from "../hooks/window";
+
 interface DemoProps {
     name: string;
 }
 
 const Demo: React.FC<DemoProps> = (props: DemoProps) => {
     const name = props.name;
+
+    const { height, width } = useWindowDimensions();
+
     return (
-        <section className="phx-hero">
-            <h1>Demo - {name}</h1>
-        </section>
+        <div id="map" style={{height: height - 56, width: "100%", backgroundColor: "blue"}}>
+            {/*<MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>*/}
+            {/*    <TileLayer*/}
+            {/*        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'*/}
+            {/*        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"*/}
+            {/*    />*/}
+            {/*    <Marker position={[51.505, -0.09]}>*/}
+            {/*        <Popup>*/}
+            {/*            A pretty CSS3 popup. <br /> Easily customizable.*/}
+            {/*        </Popup>*/}
+            {/*    </Marker>*/}
+            {/*</MapContainer>*/}
+        </div>
     );
 };
 
