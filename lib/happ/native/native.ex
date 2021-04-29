68dc03313d80451de73758cc6b973820056a0279
Tomas Korcak <korczis@gmail.com>
2021-04-29 17:36:18 +0200

    More rustler examples

--- lib/happ/native/native.ex
+++ lib/happ/native/native.ex
@@ -1,11 +1,44 @@
+defmodule AddStruct do
+  @moduledoc """
+  Result metadata.
+  """
+
+  defstruct [
+    lhs: nil,
+    rhs: nil
+  ]
+
+  @type t() :: %__MODULE__{
+                 lhs: integer(),
+                 rhs: integer(),
+               }
+end
+
 defmodule Happ.Native do
 
   @moduledoc """
   Stub for native functions implemented using Rustler.
   """
 
-  use Rustler, otp_app: :happ, crate: "happ_native"
+  use Rustler,
+      otp_app: :happ,
+      crate: "happ_native"
 
-  # When your NIF is loaded, it will override this function.
-  def add(_a, _b), do: :erlang.nif_error(:nif_not_loaded)
+  defp err do
+    throw(NifNotLoadedError)
+  end
+
+  # When your NIF is loaded, it will override these functions.
+
+  # Basic math functions
+  def add(_, _), do: err()
+  def div(_, _), do: err()
+  def mul(_, _), do: err()
+  def sub(_, _), do: err()
+
+  # Advanced math functions
+  def sum_list(_), do: err()
+
+  # Struct functions
+  def struct_echo(_), do: err()
 end
--- native-rust/happ_native/src/lib.rs
+++ native-rust/happ_native/src/lib.rs
@@ -1,26 +1,73 @@
-#[macro_use] extern crate rustler;
-use rustler::{Encoder, Env, Error, Term};
+use rustler::{Error, ListIterator, NifResult};
+use rustler::{NifStruct};
 
-mod atoms {
-    rustler_atoms! {
-        atom ok;
-        //atom error;
-        //atom __true__ = "true";
-        //atom __false__ = "false";
+// Basic math functions
+
+#[rustler::nif]
+fn add(a: i64, b: i64) -> i64 {
+    a + b
+}
+
+#[rustler::nif]
+fn div(a: i64, b: i64) -> f32 {
+    (a as f32) / (b as f32)
+}
+
+#[rustler::nif]
+fn mul(a: i64, b: i64) -> i64 {
+    a * b
+}
+
+#[rustler::nif]
+fn sub(a: i64, b: i64) -> i64 {
+    a - b
+}
+
+// Advanced math functions
+
+#[rustler::nif]
+pub fn sum_list(iter: ListIterator) -> NifResult<i64> {
+    let res: Result<Vec<i64>, Error> = iter.map(|x| x.decode::<i64>()).collect();
+
+    match res {
+        Ok(result) => Ok(result.iter().sum::<i64>()),
+        Err(err) => Err(err),
     }
 }
 
-rustler::rustler_export_nifs! {
-    "Elixir.Happ.Native",
-    [
-        ("add", 2, add)
-    ],
-    None
+// Struct functions
+
+#[derive(Debug, NifStruct)]
+#[must_use] // Added to test Issue #152
+#[module = "AddStruct"]
+pub struct AddStruct {
+    lhs: i32,
+    rhs: i32,
 }
 
-fn add<'a>(env: Env<'a>, args: &[Term<'a>]) -> Result<Term<'a>, Error> {
-    let num1: i64 = args[0].decode()?;
-    let num2: i64 = args[1].decode()?;
+#[rustler::nif]
+pub fn struct_echo(add_struct: AddStruct) -> AddStruct {
+    add_struct
+}
 
-    Ok((atoms::ok(), num1 + num2).encode(env))
+rustler::init!(
+    "Elixir.Happ.Native",
+    [
+        // Basic math functions
+        add,
+        div,
+        mul,
+        sub,
+
+        // Advanced math functions
+        sum_list,
+
+        // Struct functions
+        struct_echo
+    ],
+    load = load
+);
+
+fn load(_: rustler::Env, _: rustler::Term) -> bool {
+    true
 }
