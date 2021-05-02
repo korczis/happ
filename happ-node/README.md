dcecaa1af2abafa19aa6739a080395dc0fc5c1d9
Tomas Korcak <korczis@gmail.com>
2021-05-02 13:10:58 +0200

    Add happ-node

---
+++ happ-node/README.md
@@ -0,0 +1,1 @@
+## Happ - Node Wrapper / Implementation
---
+++ happ-node/napi/addon.js
@@ -0,0 +1,9 @@
+var addon = require('bindings')('addon.node')
+
+function add(a, b) {
+    return addon.add(a, b)
+}
+
+module.exports = {
+    add
+}
