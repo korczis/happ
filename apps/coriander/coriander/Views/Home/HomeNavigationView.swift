ec0733bcd81f877b99fda2732074a1fa64ce082e
Tomas Korcak <korczis@gmail.com>
2021-05-22 14:57:16 +0200

    Create HomeNavigationView

--- apps/coriander/coriander/Views/Home/HomeNavigationView.swift
+++ apps/coriander/coriander/Views/Home/HomeNavigationView.swift
@@ -13,72 +13,12 @@ struct HomeNavigationView: View {
     @ObservedObject var state: ObservableState<AppState>
     
     var body: some View {
-        NavigationView {
-            List() {
-                // -----
-                
-                
-                NavigationLink(
-                    destination: MapView(state: state)
-                        .navigationTitle("Map")
-                ) {
-                    HStack {
-                        Image(systemName: "map")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            // .scaledToFit()
-                        
-                        Text("Map")
-                            .padding(.leading, 15)
-                            .font(.title)
-                    }
-                    .frame(maxHeight: 25)
-                }
-                
-                
-                // -----
-                
-                
-                NavigationLink(
-                    destination: DataView(state: state)
-                        .navigationTitle("Data")
-                ) {
-                    HStack {
-                        Image(systemName: "list.dash")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            // .scaledToFit()
-                        
-                        Text("Data")
-                            .padding(.leading, 15)
-                            .font(.title)
-                            .lineLimit(2)
-                    }
-                    .frame(maxHeight: 25)
-                }
-                
-                // -----
-                
-                
-                NavigationLink(
-                    destination: SettingsView()
-                        .navigationTitle("Settings")
-                ) {
-                    HStack {
-                        Image(systemName: "gearshape")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            // .scaledToFit()
-                        
-                        Text("Settings")
-                            .padding(.leading, 15)
-                            .font(.title)
-                            .lineLimit(2)
-                    }
-                    .frame(maxHeight: 25)
-                }
-            }
-            .navigationBarTitle("Home", displayMode: .inline)
+        VStack(spacing: 0) {
+            HomeNavigationListView(state: state)
+                            
+            Spacer()
+            
+            StatusBarView(state: state)
         }
     }
 }
